75b44701519ab83e10ce4753b7a5c63a545c4a16
Mohammed Zaheen <mzaheen610@gmail.com>
2021-06-19 21:27:01 +0530

    Solution to sql assignment

---
+++ create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE publisher (
+	id integer primary key,
+	name text,
+	country text
+);
+
+CREATE TABLE books (
+	id integer primary key,
+	title text,
+	publisher integer references publisher(id)
+);
+
+CREATE TABLE subjects (
+	id integer primary key,
+	name text
+);
+
+CREATE TABLE books_subjects (
+	book integer references books(id),
+	subject integer references subjects(id)
+);
